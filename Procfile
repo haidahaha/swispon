7165c9d777e5f1dde4d3648d94d19ca3639e1b3b
Dang Nguyen <haidang.4513@gmail.com>
2015-04-12 21:47:04 +0200

    fix unicorn config

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: bundle exec unicorn -p 8080 -c ./config/unicorn.rb
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,22 @@
-# Set the working application directory
-# working_directory "/path/to/your/app"
-working_directory "/home/ubuntu/rails"
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
 
-# Unicorn PID file location
-# pid "/path/to/pids/unicorn.pid"
-pid "/home/ubuntu/rails/pids/unicorn.pid"
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
 
-# Path to logs
-# stderr_path "/path/to/log/unicorn.log"
-# stdout_path "/path/to/log/unicorn.log"
-stderr_path "/home/ubuntu/rails/log/unicorn.log"
-stdout_path "/home/ubuntu/rails/log/unicorn.log"
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
 
-# Unicorn socket
-listen "/tmp/unicorn.rails.sock"
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
 
-# Number of processes
-# worker_processes 4
-worker_processes 2
-
-# Time-out
-timeout 30
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end

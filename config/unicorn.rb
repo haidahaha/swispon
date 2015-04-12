d1650badae7afd43be13e6592954d5dad6ff4ce1
Dang Nguyen <haidang.4513@gmail.com>
2015-04-12 22:08:17 +0200

    update Unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,20 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/home/ubuntu/rails"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/home/ubuntu/rails/pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/home/ubuntu/rails/log/unicorn.log"
+stdout_path "/home/ubuntu/rails/log/unicorn.log"
+
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
